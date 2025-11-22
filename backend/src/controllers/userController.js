6731054dd66ac960501289288baeccb0a80fe8d2
HuyVo117 <nhathuy1172005@gmail.com>
2025-11-23 04:28:25 +0700

    feat: initialize frontend with React, Vite, and Tailwind CSS
    - Set up main application structure with routing for SignIn, SignUp, and ChatApp pages.
    - Add SVG asset for React logo.
    - Create basic styles using Tailwind CSS and custom CSS.
    - Implement basic page components for SignIn, SignUp, and ChatApp.
    - Configure TypeScript with appropriate settings for app and node environments.
    - Set up Vite configuration with React and Tailwind CSS plugins.

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,3 +1,10 @@
 export const authMe = async (req, res) => { 
-    return res.status(200).json({ message: "Auth me works!" });
+    try {
+        const user = req.user; // lay user da duoc gan trong middleware xac thuc
+        return res.status(200).json(user);
+    } catch (error) {
+        console.error("Loi khi lay thong tin user trong authMe: ", error);
+        return res.status(500).json({ message: "Loi He Thong" });
+        
+    }
 };
