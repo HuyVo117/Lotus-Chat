c4e079ee9918bcc62be2e4f0ac08e671e3f6bbdb
HuyVo117 <nhathuy1172005@gmail.com>
2026-01-04 21:09:04 +0700

    feat: implement user authentication, search by username, and avatar upload functionality

--- backend/src/routes/userRoute.js
+++ backend/src/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { get } from "mongoose";
 import { authMe } from "../controllers/userController.js";
-
+import { searchUserByUsername } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/me", authMe);
+router.get("/search", searchUserByUsername);
 export default router;
